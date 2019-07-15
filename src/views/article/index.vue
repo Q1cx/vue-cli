27bd736cc0a03e68623b8ca404e849d4850a5e07
longdark@126.com <longdark@126.com>
2019-07-15 17:54:42 +0800

    列表渲染

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -46,14 +46,39 @@
         根据筛选条件宫查询到
         <strong>0</strong>条结果:
       </div>
+
       <el-table :data="articles">
         <el-table-column label="封面">
-          
+          <template slot-scope="scope">
+            <el-image lazy :src="scope.row.cover.images[0]" style="width:100px;height:75px;">
+              <div slot="error" class="image-slot">
+                <img src="../../assets/images/error.gif" alt width="100px" height="75px" />
+              </div>
+            </el-image>
+          </template>
         </el-table-column>
-        <el-table-column label="标题"></el-table-column>
-        <el-table-column label="状态"></el-table-column>
-        <el-table-column label="发布时间"></el-table-column>
-        <el-table-column label="操作"></el-table-column>
+
+        <el-table-column label="标题" prop="title"></el-table-column>
+
+        <el-table-column label="状态" prop="status">
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.status===0" type="info">草稿</el-tag>
+            <el-tag v-if="scope.row.status===1">待审核</el-tag>
+            <el-tag v-if="scope.row.status===2" type="success">审核通过</el-tag>
+            <el-tag v-if="scope.row.status===3" type="warning">审核失败</el-tag>
+            <el-tag v-if="scope.row.status===4" type="danger">已删除</el-tag>
+          </template>
+        </el-table-column>
+
+        <el-table-column label="发布时间" prop="pubdate"></el-table-column>
+
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button icon="el-icon-edit" plain circle type="primary"></el-button>
+            <el-button icon="el-icon-delete" plain circle type="danger"></el-button>
+          </template>
+        </el-table-column>
+
       </el-table>
       <div class="box">
         <el-pagination background layout="prev, pager, next" :total="1000"></el-pagination>
@@ -86,7 +111,9 @@ export default {
   created () {
     // 获取频道数据
     this.getChannelOptions()
+    this.getArticles()
   },
+  // 获取列表数据
   methods: {
     async getChannelOptions () {
       // const o ={data:{}}: const {data} = o;一层解构  对象的结构一层
@@ -95,6 +122,14 @@ export default {
         data: { data }
       } = await this.axios.get('channels')
       this.channelOptions = data.channels
+    },
+    async getArticles () {
+      // post 参数  axios.post('url',(参数对象))
+      // get 传参   axios.get('url',{params:{参数对象}})
+      const {
+        data: { data }
+      } = await this.axios.get('articles', { params: this.reqParams })
+      this.articles = data.results
     }
   }
 }
@@ -105,7 +140,7 @@ export default {
 .el-card {
   margin-bottom: 20px;
 }
-.box{
+.box {
   margin-top: 20px;
   text-align: center;
 }
