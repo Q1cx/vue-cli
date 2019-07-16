7715659121aec3d21cf7ea6c624b2648ccaef989
longdark@126.com <longdark@126.com>
2019-07-16 15:02:13 +0800

    子传父

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,12 @@
 import MyBread from '@/components/my-bread.vue'
+import MyChannel from '@/components/my-channel.vue'
+
 // 封装成插件的形式,vue(插件)执行插件的配置对象install函数（安装）,且传入Vue对象。
 export default {
   install (Vue) {
     // 注册所有组件在vue下
     Vue.component(MyBread.name, MyBread)
+    Vue.component(MyChannel.name, MyChannel)
     // 多个组件在下面挨个注册即可
   }
 }
--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="container">
+    <my-channel @input="fn"></my-channel>
+    {{testData}}
     <!-- 筛选 -->
     <el-card>
       <div slot="header">
@@ -100,6 +102,7 @@
 export default {
   data () {
     return {
+      testData: '',
       // 提交给后台的条件， 传参
       // 数据默认是'空'还是null的区别
       // 空 数据会发给后台，   null  不会发字段给后台
@@ -128,6 +131,10 @@ export default {
     this.getArticles()
   },
   methods: {
+    fn (data) {
+      console.log('fn')
+      this.testData = data
+    },
     edit (id) {
       this.$router.push('/publish?id=' + id)
       // this.$router.push({ path: 'publish' , query: { id } })
